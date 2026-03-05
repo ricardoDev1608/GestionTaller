adb3ccea11534f78e6323fe253777a8ae923296b
ricardoDev1608 <146326523+ricardoDev1608@users.noreply.github.com>
2026-03-05 12:58:29 +0100

    Creacion 1ra view

--- gestionTaller_project/app_gestionTaller/urls.py
+++ gestionTaller_project/app_gestionTaller/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gestion/', include('app_gestion_coches.urls')),
+    path('clientes/', lista_clientes, name='lista_clientes'),
+    path('clientes/<int:cliente_id>/', detalle_cliente, name='detalle_cliente'),
 ]
--- gestionTaller_project/app_gestionTaller/views.py
+++ gestionTaller_project/app_gestionTaller/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Cliente
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+import json
+from .models import Cliente, Coche, Servicio, CocheServicio
 
-# Create your views here.
+
+def lista_clientes(request):
+    clientes = list(Cliente.objects.values("id", "nombre", "telefono", "email"))
+    return JsonResponse(clientes, safe=False)
+
+def detalle_cliente(request, cliente_id):
+    try:
+        cliente = Cliente.objects.values("id", "nombre", "telefono", "email").get(id=cliente_id)
+        return JsonResponse(cliente)
+    except Cliente.DoesNotExist:
+        return JsonResponse({"error": "Cliente no encontrado"}, status=404)
